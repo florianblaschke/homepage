92f05b7823260360841ff9284d81d374b9d1e8be
Florian Blaschke <florianblaschke@web.de>
2025-10-07 21:35:57 +0200

    add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import Menubar from '$lib/components/custom/menubar.svelte';
+	import '../app.css';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <div class="flex min-h-screen flex-col">
 	<Menubar />
